6935fa5725aa40fab53a5df01f666f47e63f2789
OmAryan04 <omaryan392@gmail.com>
2025-08-15 19:47:46 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.0.0",
+    author="Om Aryan",
+    author_email="omaryan392@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
